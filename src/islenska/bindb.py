28ec13849cc16e2dc6b27afb7fd841994a78afa9
Vilhjalmur Thorsteinsson <vt@extrada.com>
2021-07-13 17:45:47 +0000

    Fixed pylance complaint

--- src/islenska/bindb.py
+++ src/islenska/bindb.py
@@ -81,6 +81,7 @@ from .bincompress import BinCompressed
 
 # Type variable allowing BinEntry and Ksnid for its value
 _T = TypeVar("_T", BinEntry, Ksnid)
+_T_cov = TypeVar("_T_cov", BinEntry, Ksnid, covariant=True)
 
 ResultTuple = Tuple[str, List[_T]]
 
@@ -98,8 +99,8 @@ KsnidList = List[Ksnid]
 KsnidIterable = Iterable[Ksnid]
 
 
-class LookupFunc(Protocol[_T]):
-    def __call__(self, key: str, compound: bool = False) -> List[_T]:
+class LookupFunc(Protocol[_T_cov]):
+    def __call__(self, key: str, compound: bool = False) -> List[_T_cov]:
         ...
 
 

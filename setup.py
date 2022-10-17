82ab4e0c0f0d1118ae7951539724ca0c87a7ece0
Logi E <logi.eyjolfsson@gmail.com>
2022-10-17 11:06:57 +0000

    Remove references to Python 3.6

--- setup.py
+++ setup.py
@@ -50,8 +50,8 @@ from setuptools import find_packages  # type: ignore
 from setuptools import setup  # type: ignore
 
 
-if sys.version_info < (3, 6):
-    print("BinPackage requires Python >= 3.6")
+if sys.version_info < (3, 7):
+    print("BinPackage requires Python >= 3.7")
     sys.exit(1)
 
 # Load version string from file
@@ -104,10 +104,10 @@ setup(
         "Natural Language :: Icelandic",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Libraries :: Python Modules",
